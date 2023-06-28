7757ef55372fcc4e15f94da473c07efa2944094b
jyostna-123 <137049003+jyostna-123@users.noreply.github.com>
2023-06-28 18:19:56 +0800

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,14 @@
+package org.system;
+
+public class BankInfo {
+	public void saving() {
+		System.out.println("saving");
+	}
+	public void fixed() {
+		System.out.println("fixed");
+	}
+	public void deposit() {
+		System.out.println("deposit");
+	}
+
+}
---
+++ Desktop.java
@@ -0,0 +1,16 @@
+package org.system;
+
+import week2.day3.MyPhone;
+
+public class Desktop extends Computer {
+	public void desktopSize() {
+		System.out.println("desktopSize");
+	}
+
+	public static void main(String[] args) {
+		Desktop dp=new Desktop();
+		dp.computerModel();
+
+	}
+
+}
