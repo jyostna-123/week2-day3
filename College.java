e08d97c94bb2c319a22f5d01a7f34a7a155e1dcc
jyostna-123 <137049003+jyostna-123@users.noreply.github.com>
2023-06-28 18:17:57 +0800

    Add files via upload

---
+++ College.java
@@ -0,0 +1,15 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("NTU");
+	}
+	public void collegeCode() {
+		System.out.println("NTU03219");
+	}
+	public void collegeRank() {
+		System.out.println("5th");
+	}
+
+
+}
